90608a15b2b7e83a87b256c50d80e4fd43468d54
Naveen G <naveeng2402@gmail.com>
2022-07-10 23:30:05 +0530

    added delete text styles

--- src/dev_scripts.ts
+++ src/dev_scripts.ts
@@ -1,4 +1,5 @@
 export const delete_styles = () => {
   figma.getLocalPaintStyles().forEach((child) => child.remove());
+  figma.getLocalTextStyles().forEach((child) => child.remove());
   console.log("Styles Deleted Successfully");
 };
